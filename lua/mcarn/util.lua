dc89f40336b627cbc27b35e0ccfe8bcc268608e7
Miguel Carneiro <mcarneiromorenas@gmail.com>
2022-09-01 20:52:51 +0100

    Formatting Lua

--- lua/mcarn/util.lua
+++ lua/mcarn/util.lua
@@ -1,19 +1,19 @@
 local M = {}
 
 function M.map(m, k, v)
-	vim.keymap.set(m, k, v, { silent = true })
+  vim.keymap.set(m, k, v, { silent = true })
 end
 
 function M.nmap(k, v)
-	M.map("n", k, v)
+  M.map("n", k, v)
 end
 
 function M.vmap(k, v)
-	M.map("v", k, v)
+  M.map("v", k, v)
 end
 
 function M.imap(k, v)
-	M.map("i", k, v)
+  M.map("i", k, v)
 end
 
 return M
