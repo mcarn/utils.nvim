860917546340dfe75c494eec08102c05afa17c5b
Miguel Carneiro <mcarneiromorenas@gmail.com>
2023-05-20 19:23:03 +0100

    added luadoc

--- lua/mcarn/loader.lua
+++ lua/mcarn/loader.lua
@@ -1,4 +1,12 @@
+--- Loader module
+-- @module loader
+-- @alias M
+
 local M = {}
+
+-- Added required loader
+-- @param the module to load
+-- @usage local module = reqload("module_name")
 function M.reqload(name)
     local ok, _ = pcall(require, name)
     if not ok then
--- lua/mcarn/mapper.lua
+++ lua/mcarn/mapper.lua
@@ -1,19 +1,45 @@
+--- Mapper module
+-- @module mapper 
+-- @alias M
 local M = {}
+
 local options = { silent = true }
 
+
+-- Generic map function 
+-- @param mode vim mode n, v, i
+-- @param lhs left hand side of the mapping
+-- @param rhs right hand side of the mapping
+-- @param opts opt arguments
+-- @usage map('n',)
 function M.map(mode, lhs, rhs, opts)
     opts = opts or options
     vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+-- Normal map function 
+-- @param lhs left hand side of the mapping
+-- @param rhs right hand side of the mapping
+-- @param opts opt arguments
+-- @usage nmap('j','jzz')
 function M.nmap(lhs, rhs, opts)
     M.map("n", lhs, rhs, opts)
 end
 
+-- Visual map function 
+-- @param lhs left hand side of the mapping
+-- @param rhs right hand side of the mapping
+-- @param opts opt arguments
+-- @usage vmap('j','jzz')
 function M.vmap(lhs, rhs, opts)
     M.map("v", lhs, rhs, opts)
 end
 
+-- Insert map function 
+-- @param lhs left hand side of the mapping
+-- @param rhs right hand side of the mapping
+-- @param opts opt arguments
+-- @usage imap('j','jzz')
 function M.imap(lhs, rhs, opts)
     M.map("i", lhs, rhs, opts)
 end
